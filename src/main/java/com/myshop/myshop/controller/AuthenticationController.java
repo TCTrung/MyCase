23766ee49afba148cfbe7bbd482a53259b76436a
TCTrung <trungtrinh1325@gmail.com>
2024-04-14 22:49:07 +0700

    done encrypt password

--- src/main/java/com/myshop/myshop/controller/AuthenticationController.java
+++ src/main/java/com/myshop/myshop/controller/AuthenticationController.java
@@ -1,8 +1,14 @@
 package com.myshop.myshop.controller;
 
+import com.myshop.myshop.dto.request.ApiResponse;
+import com.myshop.myshop.dto.request.AuthenticationRequest;
+import com.myshop.myshop.dto.response.AuthenticationResponse;
+import com.myshop.myshop.service.AuthenticationService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AuthenticationController {
+    AuthenticationService authenticationService;
 
+    @PostMapping("/log-in")
+    ApiResponse<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request){
+        boolean result = authenticationService.authenticate(request);
+        return ApiResponse.<AuthenticationResponse>builder()
+                .result(AuthenticationResponse.builder()
+                        .authenticated(result)
+                        .build())
+                .build();
+    }
 }
--- src/main/java/com/myshop/myshop/service/AuthenticationService.java
+++ src/main/java/com/myshop/myshop/service/AuthenticationService.java
@@ -1,10 +1,14 @@
 package com.myshop.myshop.service;
 
 import com.myshop.myshop.dto.request.AuthenticationRequest;
+import com.myshop.myshop.exception.AppException;
+import com.myshop.myshop.exception.ErrorCode;
 import com.myshop.myshop.repository.UserRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,7 +16,11 @@ import org.springframework.stereotype.Service;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AuthenticationService {
     UserRepository userRepository;
-    boolean authenticate (AuthenticationRequest request){
-        var user = userRepository.f
+    public boolean authenticate (AuthenticationRequest request){
+        var user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED));
+
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
+        return passwordEncoder.matches(request.getPassword(), user.getPassword());
     }
 }
